717527476dc7959e4346aba76065de528d2c272b
giovm <giova1918@hotmail.it>
2017-01-03 13:22:41 +0100

    Ciao mbare Angi

--- Hey Hi/src/com/heyhi/main/Main.java
+++ Hey Hi/src/com/heyhi/main/Main.java
@@ -8,7 +8,7 @@ public interface Main {
 		//Voice.getAvailableVoices().stream().forEach(System.out::println);
 		tts.setVoice("istc-lucia-hsmm");
 		tts.setAudioEffect("Rate(durScale:2.0)");
-		tts.speak("Ciao Angelo Marini", 2.0f, false, true);
+		tts.speak("Ciao Angelo", 2.0f, false, true);
 		
 		
 	}
